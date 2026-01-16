892d23d5486e6a57281ff5e089ebd37314f2adea
Shiron <contact@shiron.io>
2026-01-16 17:05:51 +0100

    Initial Commit

---
+++ src/Program.cs
@@ -0,0 +1,8 @@
+﻿using Silk.NET.Windowing;
+
+var options = WindowOptions.Default;
+options.Title = "Vulkan Dumpster Project";
+options.Size = new Silk.NET.Maths.Vector2D<int>(1920, 1080);
+
+var window = Window.Create(options);
+window.Run();
