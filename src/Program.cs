72b2de1536be74f1bb0a0a18610eb4f9b5aa100e
Shiron <contact@shiron.io>
2026-01-16 17:12:35 +0100

    refactor: Refactor program class, added on key down to input context

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,32 @@
-﻿using Silk.NET.Windowing;
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
 
-var options = WindowOptions.Default;
-options.Title = "Vulkan Dumpster Project";
-options.Size = new Silk.NET.Maths.Vector2D<int>(1920, 1080);
+public class Program {
+    private static IWindow _window = null!;
 
-var window = Window.Create(options);
-window.Run();
+    public static void Main(string[] args) {
+        var options = WindowOptions.Default;
+        options.Title = "Vulkan Dumpster Project";
+        options.Size = new Vector2D<int>(1920, 1080);
+
+        _window = Window.Create(options);
+
+        _window.Load += () => {
+            Console.WriteLine("Window loaded");
+            var inputContext = _window.CreateInput();
+
+            foreach (var kb in inputContext.Keyboards) {
+                kb.KeyDown += KeyDown;
+            }
+        };
+
+        _window.Run();
+    }
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int arg3) {
+        if (key == Key.Escape) {
+            _window.Close();
+        }
+    }
+}
